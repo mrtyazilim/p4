c3d76dda91834161f6023b59fe0892682936a5a5
Murat.bt <bt.mrt@yandex.com>
2024-03-19 13:39:41 +0300

    online da oluşan dosya ,dosya tarayıcıdında çıkmıyordu. şimdi sanki varmışcasına git menusundan işle ve göndere basalım

---
+++ src/VarliksalSiniflar/UserV_2.java
@@ -0,0 +1,41 @@
+package VarliksalSiniflar;
+
+/**
+ @author Murat Değirmenci,UPoDer
+ 
+ */
+
+/*
+1.get , set, üye değişken tanımlamaları için ide destekleri öğrenilmeli
+git git hub öğrenilmeli
+
+*/
+public class UserV_2 {
+    private String username,password,name,surname;
+    //public char[] tckn= {'1','1','2','2','3','4','4','5','5','5','5'};
+    private char[] tckn=new char[11];
+    // burası belki de string olmalı
+    //tarih saat ekle
+    //zorunlu alanarı nasıl yapmalıyım
+    private String date,time;
+    enum gender {        Kadin,        Erkek    };
+
+
+    // bilerek soy adı zorunlu tutmadım.
+    public UserV_2(String username,String password, String tckn,
+    String name){
+        this.username=username;
+        this.password=password;
+        this.tckn=tckn;
+        //burası özel ilgi istiyaor
+        this.name;
+
+
+    }
+
+public void setDateTime(){
+    
+}
+
+
+}
