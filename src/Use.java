907327312e181bf51e11e8eaa5e0b90f77eabba8
mrtyazilim <bt.mrt@yandex.com>
2024-03-21 10:36:48 +0300

    ShowEntrance();

--- src/Use.java
+++ src/Use.java
@@ -1,2 +1,16 @@
 public class Use {
+    public static void main(String[] args) {
+        ShowEntrance();
+    }
+
+    static void ShowEntrance(){
+        //System.out.printf("Hoşgeldiniz. \\n Lütfen rakamlar yardımıyla seçiminizi yapınız.");
+        System.out.println("1 :Üye oturum girişi. \\n 2.Üye ol. ");
+    }
+//    static void askUserInfo(){
+//
+//
+//    }
+//
+
 }
