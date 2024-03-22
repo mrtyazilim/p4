e37824f07ca53a31fd68178a2e15ed95ca2e7fff
mrtyazilim <bt.mrt@yandex.com>
2024-03-22 09:49:27 +0300

    ilk seçim soruldu

--- src/Use.java
+++ src/Use.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Use {
+   static Scanner choiceScanner=new Scanner(System.in);
+    static char choice;
     public static void main(String[] args) {
         ShowEntrance();
+        choice=choiceScanner.nextLine().charAt(0);
+
     }
 
     static void ShowEntrance(){
-        //System.out.printf("Hoşgeldiniz. \\n Lütfen rakamlar yardımıyla seçiminizi yapınız.");
-        System.out.println("1 :Üye oturum girişi. \\n 2.Üye ol. ");
+        System.out.println("Hoşgeldiniz. \n Lütfen rakamlar yardımıyla seçiminizi yapınız.");
+        System.out.println("1 :Üye oturum girişi.\n2 :Üye ol. ");
+    }
+    static void ShowUserCredidental(){///Ask
+        System.out.println("Kullanıcı adı :");
+        System.out.println("Parola :");
     }
 //    static void askUserInfo(){
 //
