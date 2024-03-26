4da97a2786a332098a8425f262317401ef97c015
Murat.bt <bt.mrt@yandex.com>
2024-03-26 14:40:50 +0300

    Update Dataable.java

--- src/VeriselTipler/Dataable.java
+++ src/VeriselTipler/Dataable.java
@@ -2,6 +2,8 @@ package VeriselTipler;
 /**
 veri eklerken nesneleri gruplamak için.
   bu sebeple henüz metodu yok.
+  sadece polymorfizm için var şimdilik
+  
 */
 //import FileNotFoundException;
 //import java.time.Clock;
